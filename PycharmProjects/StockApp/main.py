6e0e93aca5ba80a4235bdd62d933fddbca752bf4
reurc <ricardo.escobaru@gmail.com>
2018-12-29 18:46:58 -0600

    Remove calculation based on purchase date, rewrite for readability

--- PycharmProjects/StockApp/main.py
+++ PycharmProjects/StockApp/main.py
@@ -7,11 +7,7 @@ def getJSONAsString(symbol):
     url = urlopen('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + symbol +
                   '&outputsize=full&apikey=KIH6VFJ03Z89T5DI')
     str_response = url.read()
-    if b"Invalid" in str_response:
-        print("Invalid API call!")
-        exit(1)
-    else:
-        return str_response
+    return str_response
 
 def sharesKnown(shares, last_price):
     value = last_price*shares
@@ -27,26 +23,89 @@ def sharesNotKnown(purchase_cost, purchase_date, last_price, jsonAsString):
     value = last_price * shares
     return value
 
-try:
-    symbol = input('Please enter symbol name: ')
-    str_response = getJSONAsString(symbol)
+def singleHoldingMode():
+    while True:
+        symbol = input('Please enter symbol name: ')
+        str_response = getJSONAsString(symbol)
+        if b"Invalid" not in str_response:
+            break
+        else:
+            print("Invalid API call! Try again.")
+
     result = json.loads(str_response)
     last_updated = result['Meta Data']['3. Last Refreshed']
     last_price = float(result['Time Series (Daily)'][last_updated]['4. close'])
+    shares = float(input('Please enter number of shares: '))
+    value = sharesKnown(shares, last_price)
 
-    answer = input('Do you know how many shares you purchased? Enter "Y" or "N": ')
-
-    if answer.lower() == 'y':
-        shares = float(input('Please enter number of shares: '))
-        value = sharesKnown(shares, last_price)
-    elif answer.lower() == 'n':
-        purchase_cost = float(input('If you don\'t know how many shares you purchased, enter your total stock purchase '
-                                     'cost: $'))
-        purchase_date = input('Now enter the purchase date in YYYY-MM-DD format: ')
-        value = sharesNotKnown(purchase_cost, purchase_date, last_price, result)
-    else:
-        print('You did not enter "Y" or "N". You\'re gonna have to start over!')
-        exit(1)
     print(f'Using data from ' + last_updated + ', your holdings are worth $%.2f.' % value)
+    return
+
+def initializeTable(symbolSet):
+    table = {}
+    for symbol in symbolSet:
+        table[symbol] = 0
+    return table
+
+def updateTable(table):
+    for key in table:
+        while True:
+            shares = input('How many shares of ' + key.upper() + ' do you own? ')
+            try:
+                number = int(shares)
+                if number <= 0:
+                    print("That number was not positive, please try again.")
+                else:
+                    table[key] = shares
+                    break
+            except ValueError:
+                print("This was not a number, please try again.")
+
+    return table
+
+
+def portfolioMode():
+    print('You have selected Portfolio mode. To start, enter all of your portolio\'s symbols. \n'
+          'We will let you know if we can\'t find the symbol you entered and ask you to try again. \n'
+          'When you\'re done entering symbols, please enter "Done"\n')
+
+    symbolSet = []
+
+    while True:
+        symbol = input('Please enter symbol name: ')
+        if symbol.lower() == "done":
+            break
+        else:
+            if symbol.upper() not in symbolSet:
+                str_response = getJSONAsString(symbol)
+                if b"Invalid" not in str_response:
+                    # add symbol to symbol list
+                    print("Found recent market data for " + symbol.upper() + "!")
+                    symbolSet.append(symbol.upper())
+                else:
+                    print("Invalid API call! Try again.")
+            else:
+                print(symbol.upper() + " was already in the symbol list.")
+
+    if len(symbolSet) == 0:
+        print("You have given us an empty symbol list. Your total portfolio value is $0.00!")
+    else:
+        print("the list is not empty")
+        print(symbolSet)
+        table = initializeTable(symbolSet)
+        print(table)
+        print('Now you must tell us how many shares (as integers) of each stock you own.\n')
+        table = updateTable(table)
+        print(table)
+    return
+
+try:
+    mode = input('Enter "P" for Portfolio (multiple holdings) mode or "S" for single holding mode: ')
+    if mode.lower() == 'p':
+        # do portfolio
+        portfolioMode();
+    elif mode.lower() == 's':
+        # do single holding
+        singleHoldingMode()
 except KeyboardInterrupt:
-    print("Bye!!!")
+    print("\nBye!!!")
